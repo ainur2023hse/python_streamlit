b912b4950d134d74a0dbbfb299e3b21c8bb3b971
ainur2023hse <148802637+ainur2023hse@users.noreply.github.com>
2023-12-12 19:06:36 +0500

    Update streamlit_project.py

--- streamlit_project.py
+++ streamlit_project.py
@@ -19,17 +19,16 @@ sns.heatmap(data.corr(), annot=False, vmin=-1, vmax=1, center=0, cmap='coolwarm'
 st.pyplot(fig)
 
 
-data_new = data.drop(columns=["GENDER", "SOCSTATUS_WORK_FL", "SOCSTATUS_PENS_FL", "AGREEMENT_RK"])
+data_new = data.drop(columns=["GENDER", "SOCSTATUS_WORK_FL", "SOCSTATUS_PENS_FL", "AGREEMENT_RK", "TARGET"])
 description = data_new.describe()
 st.dataframe(description)
 
 
 fig = plt.figure()
 fig.suptitle("Classification graphs")
-data_new = data.drop(columns=["GENDER", "SOCSTATUS_WORK_FL", "SOCSTATUS_PENS_FL", "AGREEMENT_RK", "TARGET"])
+data_new = data.drop(columns=["GENDER", "SOCSTATUS_WORK_FL", "SOCSTATUS_PENS_FL", "AGREEMENT_RK"])
 fig = sns.pairplot(data_new, hue="TARGET")
 st.pyplot(fig)
-
 
 
 
